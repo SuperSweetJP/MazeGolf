43bec98be5b538d339f187cee9fd7a5763c33e00
SuperSweet <SuperSweet@DESKTOP-JGO5AAD>
2024-12-04 07:28:31 +0200

    initial push

---
+++ Assets/Scripts/MazeConstructor.cs
@@ -0,0 +1,315 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MazeConstructor : MonoBehaviour
+{
+    //1
+    public bool showDebug;
+    [Range(0, 1)]
+    public float placementTreshold = .68f;
+
+    public GameObject agentGO;
+    [SerializeField] private Material mazeMat1;
+    [SerializeField] private Material mazeMat2;
+    [SerializeField] public Material goalMat;
+
+    private MazeDataGenerator dataGenerator;
+    private MazeMeshGenerator meshGenerator;
+    //public RollerAgent rollerAgentScript;
+
+    public int xSize = 13;
+    public int ySize = 15;
+    public float placementThreshold = .68f;    // chance of empty space
+    public float hallWidth = 3.75f;
+    public float hallHeight = 3.5f;
+
+    public GameObject targetObject;
+    public int score = 0;
+    public int maxTargets = 4;
+    public List<GameObject> targetGOList = new List<GameObject>();
+    public bool allTargetsReached = false;
+
+    public Vector3 transformPos;
+    private GameObject agentObject;
+
+    public int[,] data
+    {
+        get; private set;
+    }
+
+    void Awake()
+    {
+        // default to walls surrounding a single empty cell
+        data = new int[,]
+        {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+
+        //spawnPoints = new Vector3[x];
+
+        dataGenerator = new MazeDataGenerator();
+        meshGenerator = new MazeMeshGenerator();
+        transformPos = transform.position;
+    }
+
+    void Start()
+    {
+        GenerateNewMaze(xSize, ySize, placementThreshold);
+        setupWorld();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Object.Destroy(agentObject);
+            foreach (GameObject i in targetGOList)
+            {
+                Object.Destroy(i);
+            }
+
+            setupWorld();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //if (this.transform.localPosition.y < 0)
+        //{
+        //    // If the Agent fell, zero its momentum
+        //    agentObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        //    agentObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        //    FindRandomPosition();
+        //    this.transform.localPosition = new Vector3(transformPos.x + randLoc.x * hallWidth, transformPos.y + this.transform.localScale.y / 2, transformPos.z + randLoc.z * hallWidth);
+        //}
+        ////When all goals reached, do something
+        //if (score == targetGOList.Count)
+        //{
+        //    //Reset maze later comes here
+        //    score = 0;
+        //    //Debug.Log(score);
+        //    //end episode?
+        //    rollerAgentScript.EndEpisode();
+        //    targetGOList.Clear();
+        //    SpawnTargets();
+        //    allTargetsReached = true;
+        //}
+    }
+
+    public void GenerateNewMaze(int sizeRows, int sizeCols, float plTresh)
+    {
+        if (sizeRows % 2 == 0 && sizeCols % 2 == 0)
+        {
+            Debug.LogError("Odd numbers work better for dungeon size.");
+        }
+
+        //DisposeOldMaze();
+
+        data = dataGenerator.FromDimensions(sizeRows, sizeCols, plTresh);
+
+        //get obstacle coordinates here
+        //Debug.Log(data);
+
+        DisplayMaze();
+    }
+
+    private void DisplayMaze()
+    {
+        GameObject go = new GameObject();
+        go.transform.position = Vector3.zero;
+        go.transform.parent = gameObject.transform;
+        go.name = "Procedural Maze";
+        go.tag = "Generated";
+
+
+        MeshFilter mf = go.AddComponent<MeshFilter>();
+        mf.mesh = meshGenerator.FromData(data, hallWidth, hallHeight, transformPos);
+
+        MeshCollider mc = go.AddComponent<MeshCollider>();
+        mc.sharedMesh = mf.mesh;
+
+        MeshRenderer mr = go.AddComponent<MeshRenderer>();
+        mr.materials = new Material[2] { mazeMat1, mazeMat2 };
+    }
+
+
+    void OnGUI()
+    {
+        //1
+        if (!showDebug)
+        {
+            return;
+        }
+        //2
+        int[,] maze = data;
+        int rMax = maze.GetUpperBound(0);
+        int cMax = maze.GetUpperBound(1);
+        string msg = "";
+        //3
+        for (int i = rMax; i >= 0; i--)
+        {
+            for (int j = 0; j <= cMax; j++)
+            {
+                if (maze[i, j] == 0)
+                {
+                    msg += "0";
+                }
+                else
+                {
+                    msg += "1";
+                }
+            }
+            msg += "\n";
+        }
+        //4
+        GUI.Label(new Rect(20, 20, 500, 500), msg);
+    }
+
+    public Vector3 randLoc
+    {
+        get; private set;
+    }
+
+    public void setupWorld()
+    {
+        SpawnTargets();
+        //place player
+        FindRandomPosition();
+        //this.transform.position = new Vector3(mazeConstructor.randLoc.x * mazeConstructor.hallWidth, this.transform.localScale.y/2, mazeConstructor.randLoc.z * mazeConstructor.hallWidth);
+        agentObject = Instantiate(agentGO, new Vector3(transformPos.x + randLoc.x * hallWidth, transformPos.y + this.transform.localScale.y / 2, transformPos.z + randLoc.z * hallWidth), Quaternion.identity);
+        agentObject.transform.parent = gameObject.transform;
+        //rollerAgentScript = agentObject.GetComponent<RollerAgent>();
+    }
+
+    public void DisposeOldMaze()
+    {
+        //this will cause problems down the line???/
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Generated");
+        foreach (GameObject go in objects)
+        {
+            Destroy(go);
+        }
+    }
+
+    public void FindRandomPosition()
+    {
+        int[,] maze = data;
+        int rMax = maze.GetUpperBound(0);
+        int cMax = maze.GetUpperBound(1);
+        int freeSpace = 0;
+
+        //get count of free spaces
+        for (int i = rMax; i >= 0; i--)
+        {
+            for (int j = 0; j <= cMax; j++)
+            {
+                if (maze[i, j] == 0)
+                {
+                    freeSpace++;
+                }
+            }
+        }
+
+        bool notValidLoc = true;
+        int xPos = 0;
+        int yPos = 0;
+
+        if(freeSpace>0)
+        {
+            while (notValidLoc)
+            {
+                xPos = Random.Range(0, rMax);
+                yPos = Random.Range(0, rMax);
+                if (maze[yPos, xPos] == 0)
+                {
+                    Vector3 v3 = new Vector3(xPos, 0, yPos);
+                    randLoc = v3;
+                    //set poition taken in the maze data array, to avoid overlap on next spawns
+                    data[yPos, xPos] = 1;
+                    notValidLoc = false;
+                    return;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Theres no free space to spawn");
+        }
+
+    }
+
+    public bool checkFreeSpaces()
+    {
+        int[,] maze = data;
+        int rMax = maze.GetUpperBound(0);
+        int cMax = maze.GetUpperBound(1);
+        int freeSpace = 0;
+        //get count of free spaces
+        for (int i = rMax; i >= 0; i--)
+        {
+            for (int j = 0; j <= cMax; j++)
+            {
+                if (maze[i, j] == 0)
+                {
+                    freeSpace++;
+                }
+            }
+        }
+        if (freeSpace > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public Vector3 GetRandVector()
+    {
+        bool notValidLoc = true;
+        int xPos = 0;
+        int yPos = 0;
+        Vector3 randV3 = new Vector3();
+        int[,] maze = data;
+        int rMax = maze.GetUpperBound(0);
+        int cMax = maze.GetUpperBound(1);
+
+        while (notValidLoc)
+        {
+            xPos = Random.Range(0, rMax);
+            yPos = Random.Range(0, rMax);
+            if (maze[yPos, xPos] == 0)
+            {
+                randV3 = new Vector3(xPos, 0, yPos);
+                //set poition taken in the maze data array, to avoid overlap on next spawns
+                data[yPos, xPos] = 1;
+                notValidLoc = false;
+            }
+        }
+        return randV3;
+    }
+
+    public void SpawnTargets()
+    { 
+        //Vector3 targetLoc = new Vector3();
+        //int i = 0;
+        //while (i < maxTargets)
+        //{
+        //    targetLoc = GetRandVector();
+        //    GameObject targetGO = Instantiate(targetObject, new Vector3(transformPos.x + targetLoc.x * hallWidth, transformPos.y + targetObject.transform.localScale.y/2, transformPos.z + targetLoc.z * hallWidth), Quaternion.identity);
+        //    //add gameobject to an array
+        //    targetGOList.Add(targetGO);
+        //    targetGO.transform.parent = gameObject.transform;
+        //    TargetObject targetObjectScript = targetGO.GetComponent<TargetObject>();
+        //    targetObjectScript.xPos = (int)targetLoc.x;
+        //    targetObjectScript.zPos = (int)targetLoc.z;
+        //    i++;
+        //}
+    }
+
+
+}
+
---
+++ Assets/Scripts/MazeMeshGenerator.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeMeshGenerator
+{
+
+    public Mesh FromData(int[,] data, float width, float height, Vector3 transformPos)
+    {
+        Mesh maze = new Mesh();
+
+        //3
+        List<Vector3> newVertices = new List<Vector3>();
+        List<Vector2> newUVs = new List<Vector2>();
+
+        maze.subMeshCount = 2;
+        List<int> floorTriangles = new List<int>();
+        List<int> wallTriangles = new List<int>();
+
+        int rMax = data.GetUpperBound(0);
+        int cMax = data.GetUpperBound(1);
+        float halfH = height * .5f;
+
+        //4
+        for (int i = 0; i <= rMax; i++)
+        {
+            for (int j = 0; j <= cMax; j++)
+            {
+                if (data[i, j] == 1 && i != 0 && i != rMax &&
+                    j != 0 && j != cMax)
+                {
+                    AddQuad(Matrix4x4.TRS(
+                        new Vector3(transformPos.x + j * width, transformPos.y + height, transformPos.z + i * width),
+                        Quaternion.LookRotation(Vector3.up),
+                        new Vector3(width, width, 1)
+                    ), ref newVertices, ref newUVs, ref floorTriangles);
+                }
+                //ja konkrētā šūna ir blank..
+                if (data[i, j] != 1)
+                {
+                    // floor
+                    AddQuad(Matrix4x4.TRS(
+                        new Vector3(transformPos.x + j * width, transformPos.y, transformPos.z + i * width),
+                        Quaternion.LookRotation(Vector3.up),
+                        new Vector3(width, width, 1)
+                    ), ref newVertices, ref newUVs, ref floorTriangles);
+
+                    // ceiling
+                    //ToDo: Only for obstacles
+                    //Te būs tikai 0 value šūnas
+                    //Debug.Log(string.Format("{0} {1}: {2}",i ,j, data[i, j]));
+
+                
+
+                    // walls on sides next to blocked grid cells
+                    //i-1<0 is corner wall OR is filled 
+                    if (i - 1 < 0 || data[i - 1, j] == 1)
+                    {
+                        AddQuad(Matrix4x4.TRS(
+                            new Vector3(transformPos.x + j * width, transformPos.y + halfH, transformPos.z + (i - .5f) * width),
+                            Quaternion.LookRotation(Vector3.forward),
+                            new Vector3(width, height, 1)
+                        ), ref newVertices, ref newUVs, ref wallTriangles);
+                    }
+
+                    if (j + 1 > cMax || data[i, j + 1] == 1)
+                    {
+                        AddQuad(Matrix4x4.TRS(
+                            new Vector3(transformPos.x + (j + .5f) * width, transformPos.y + halfH, transformPos.z + i * width),
+                            Quaternion.LookRotation(Vector3.left),
+                            new Vector3(width, height, 1)
+                        ), ref newVertices, ref newUVs, ref wallTriangles);
+                    }
+
+                    if (j - 1 < 0 || data[i, j - 1] == 1)
+                    {
+                        AddQuad(Matrix4x4.TRS(
+                            new Vector3(transformPos.x + (j - .5f) * width, transformPos.y + halfH, transformPos.z + i * width),
+                            Quaternion.LookRotation(Vector3.right),
+                            new Vector3(width, height, 1)
+                        ), ref newVertices, ref newUVs, ref wallTriangles);
+                    }
+
+                    if (i + 1 > rMax || data[i + 1, j] == 1)
+                    {
+                        AddQuad(Matrix4x4.TRS(
+                            new Vector3(transformPos.x + j * width, transformPos.y + halfH, transformPos.z + (i + .5f) * width),
+                            Quaternion.LookRotation(Vector3.back),
+                            new Vector3(width, height, 1)
+                        ), ref newVertices, ref newUVs, ref wallTriangles);
+                    }
+                }
+            }
+        }
+
+        maze.vertices = newVertices.ToArray();
+        maze.uv = newUVs.ToArray();
+
+        maze.SetTriangles(floorTriangles.ToArray(), 0);
+        maze.SetTriangles(wallTriangles.ToArray(), 1);
+
+        //5
+        maze.RecalculateNormals();
+
+        return maze;
+    }
+
+    //1, 2
+    private void AddQuad(Matrix4x4 matrix, ref List<Vector3> newVertices,
+        ref List<Vector2> newUVs, ref List<int> newTriangles)
+    {
+        int index = newVertices.Count;
+
+        // corners before transforming
+        Vector3 vert1 = new Vector3(-.5f, -.5f, 0);
+        Vector3 vert2 = new Vector3(-.5f, .5f, 0);
+        Vector3 vert3 = new Vector3(.5f, .5f, 0);
+        Vector3 vert4 = new Vector3(.5f, -.5f, 0);
+
+        newVertices.Add(matrix.MultiplyPoint3x4(vert1));
+        newVertices.Add(matrix.MultiplyPoint3x4(vert2));
+        newVertices.Add(matrix.MultiplyPoint3x4(vert3));
+        newVertices.Add(matrix.MultiplyPoint3x4(vert4));
+
+        newUVs.Add(new Vector2(1, 0));
+        newUVs.Add(new Vector2(1, 1));
+        newUVs.Add(new Vector2(0, 1));
+        newUVs.Add(new Vector2(0, 0));
+
+        newTriangles.Add(index + 2);
+        newTriangles.Add(index + 1);
+        newTriangles.Add(index);
+
+        newTriangles.Add(index + 3);
+        newTriangles.Add(index + 2);
+        newTriangles.Add(index);
+    }
+
+}
